2211e48ebd7daa7907380c44d92623d54d9f00e4
V. Bruk <vbruk@hey.com>
2023-07-26 13:02:01 -0300

    Update devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
 
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   add_flash_types :info, :error, :success, :warning
   layout :layout_by_resource
 
   def after_sign_in_path_for(_resource)
     # Establish different redirect for current_admin and currrent_user
-    # if current_admin
-    #   admin_root_path
-    # else
-    #   root_path
-    # end
+    if current_admin
+      projects_path
+    else
+      root_path
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # belogns_to :project
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable, :trackable
