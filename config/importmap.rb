e34faa3330168c20ddfa836555da33b29ca4df99
V. Bruk <vbruk@hey.com>
2023-07-26 11:52:44 -0300

    Pin some JS

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,8 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "easymde", to: "https://unpkg.com/easymde/dist/easymde.min.js", preload: true
+pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
