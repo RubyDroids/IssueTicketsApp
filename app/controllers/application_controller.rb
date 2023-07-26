8b274f677b6865067fdacf39d489755955ac4096
V. Bruk <vbruk@hey.com>
2023-07-26 12:29:47 -0300

    Install Devise models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  add_flash_types :info, :error, :success, :warning
+  layout :layout_by_resource
+
+  def after_sign_in_path_for(_resource)
+    # Establish different redirect for current_admin and currrent_user
+    # if current_admin
+    #   admin_root_path
+    # else
+    #   root_path
+    # end
+  end
+
+  private
+
+  def layout_by_resource
+    case params["controller"]
+    when "devise/sessions", "devise/registrations", "devise/passwords"
+      "devise"
+    else
+      "application"
+    end
+  end
 end
