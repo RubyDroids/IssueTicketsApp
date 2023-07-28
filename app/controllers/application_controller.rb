2e251321ac2eaff7307110c00110b938a0cb4fcd
V. Bruk <vbruk@hey.com>
2023-07-28 13:07:09 -0300

    Try another gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,19 @@
 class ApplicationController < ActionController::Base
-  include Pagy::Backend
-
-  add_flash_types :info, :error, :success, :warning
-  layout :layout_by_resource
+  before_action :set_current_request_details
+  before_action :authenticate
 
   private
 
-  def layout_by_resource
-    case params["controller"]
-    when "devise/sessions", "devise/registrations", "devise/passwords"
-      "devise"
+  def authenticate
+    if session_record = Session.find_by_id(cookies.signed[:session_token])
+      Current.session = session_record
     else
-      "application"
+      redirect_to sign_in_path
     end
+  end
+
+  def set_current_request_details
+    Current.user_agent = request.user_agent
+    Current.ip_address = request.ip
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,24 @@ require "sidekiq/web"
 Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq"
 
+  # Auth
+  get  "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  get  "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  resources :sessions, only: [:index, :show, :destroy]
+  resource  :password, only: [:edit, :update]
+  namespace :identity do
+    resource :email,              only: [:edit, :update]
+    resource :email_verification, only: [:show, :create]
+    resource :password_reset,     only: [:new, :edit, :create, :update]
+  end
+
+  # Resources
   resources :labels, only: %i[index show]
   resources :issues
   resources :projects
 
-  # Defines the root path route ("/")
+  # Main path
   root "pages#index"
 end
